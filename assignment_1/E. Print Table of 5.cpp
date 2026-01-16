f43f034235d25c86eef7517a2bbf1c1cd68a6e1a
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-16 20:09:14 +0530

    CF

--- assignment_1/E. Print Table of 5.cpp
+++ assignment_1/E. Print Table of 5.cpp
@@ -11,9 +11,9 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout << "5 * 2 = " << 5*1 << endl;
-    cout << "5 * 3 = " << 5*2 << endl;
-    cout << "5 * 1 = " << 5*3 << endl;
+    cout << "5 * 1 = " << 5*1 << endl;
+    cout << "5 * 2 = " << 5*2 << endl;
+    cout << "5 * 3 = " << 5*3 << endl;
     cout << "5 * 4 = " << 5*4 << endl;
     cout << "5 * 5 = " << 5*5 << endl;
     cout << "5 * 6 = " << 5*6 << endl;
